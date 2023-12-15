1dc110cc83b2f82d0447a9d8fcc053aea922aa61
Nilabh Kumar <74805255+Nilabh2121@users.noreply.github.com>
2023-12-15 22:03:37 +0530

    Creating Gofr server Sucessfully

---
+++ GOLANG_MINI_PROJECT/Restapi_usingGOFR.go
@@ -0,0 +1,13 @@
+package main
+
+import "gofr.dev/pkg/gofr"
+
+func main() {
+	app := gofr.New() // it initialize the gofr object
+
+	app.GET("/greetmessage", func(ctx *gofr.Context) (interface{}, error) {
+		return "Hello Gogfr from Nilabh!", nil
+	})
+
+	app.Start()
+}
